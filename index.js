ff3cad175c2ff68298e7c0fb7678474c6ab00ff3
marina.theotiste@gmail.com <73482347+marinaongithub@users.noreply.github.com>
2023-03-07 00:06:27 +0000

    Added prompts in index.js

--- index.js
+++ index.js
@@ -10,6 +10,121 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+function init() {
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+    let employeeType = "Manager";
+    let extraInfo = "office number";
 
+    let prompts = [
+            {
+                type: "input",
+                message: () => {
+
+                return `What's the ${employeeType} name?`
+            },
+                name: "name",
+                validate: (input) => {
+                    if (!input) {
+                        return `Please enter ${employeeType} name!`;
+                    }
+                    return true;
+                }
+            },
+            {
+                type: "input",
+                message: () => {
+                    return `What's the ${employeeType} id?`
+                },
+                name: "id",
+                validate: (input) => {
+                    if (!input) {
+                        return `Please enter the ${employeeType} id!`;
+                    }
+                    return true;
+                }
+            },
+            {
+                type: "input",
+                message: () => {
+                    return `What's the ${employeeType} email?`
+                },
+                name: "email",
+                validate: (input) => {
+                    if (!input) {
+                        return `Please enter the ${employeeType} email!`;
+                    }
+                    return true;
+                }
+            },
+            {
+                type: "input",
+                message: () => {
+                    return `What's the ${employeeType} ${extraInfo}?`
+                },
+                name: "extraInfo",
+                validate: (input) => {
+                    if (!input) {
+                        return "Please enter the team Manager office number";
+                    }
+                    return true;
+                }
+            },
+            {
+                type: "list",
+                message: "Which type of team member do you want to add?",
+                name: "employeeType",
+                choices: ["Engineer", "Intern", "I don't want to add another team member."],
+            },
+        ];
+
+        inquirer.prompt(prompts)
+        .then(async (response) => {
+
+            // Employee type of the new member to add
+            employeeType = response.employeeType;
+
+            // array to store the team members
+            let team = [];
+
+            // creates a new manager object
+            let manager = new Manager(response.name, response.id, response.email, response.extraInfo);
+           
+            // add the new manager object to the array
+            team.push(manager);
+
+            // while the user decides to add a new member, the questions presented with wording corresponding to the new member to add
+            while (employeeType !== "I don't want to add another team member.") {
+
+                // update extraInfo wording to 'github' if the new member to add is an engineer
+                if (employeeType === "Engineer") {
+                    extraInfo = "github";
+
+                    // prompts with updated wording
+                    newMember = await inquirer.prompt(prompts)
+
+                    // creates a new Employee, type depending on the user previous input
+                    team.push(new Engineer(newMember.name, newMember.id, newMember.email, newMember.extraInfo));
+                    console.log(newMember);
+                }
+
+                // update extraInfo wording to 'school' if the new member to add is an intern
+                else if (employeeType === "Intern") {
+                    extraInfo = "school";
+
+                    // prompts with updated wording
+                    newMember = await inquirer.prompt(prompts)
+
+                    // creates a new Employee, type depending on the user previous input
+                    team.push(new Intern(newMember.name, newMember.id, newMember.email, newMember.extraInfo));
+                    console.log(newMember);
+                }
+
+                employeeType = newMember.employeeType;
+
+                console.log(team);
+            }               
+    })
+}
+
+
+init();
